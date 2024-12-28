7eec9bce08c4d21beccfceca0cc2655e048dd715
camol23 <camol23@student.sdu.dk>
2024-12-28 17:50:00 +0100

    add bottons

--- simulate_code_matplotlib/Control_board/Board_GUI.py
+++ simulate_code_matplotlib/Control_board/Board_GUI.py
@@ -18,6 +18,7 @@ class Graphics:
         self.steel_blue = (70,130,180)
         self.royal_blue = (65,105,225)
         self.sky_blue = (135,206,235)
+        self.light_sky_blue = (135,206,250)
         self.gainsboro = (220,220,220)                              # ligth gray
         self.white_smoke = (245,245,245)     
         self.crimson = (220,20,60)     
@@ -28,6 +29,7 @@ class Graphics:
         self.slate_gray = (112,128,144)
         self.mint_cream =  	(245,255,250)
         self.navajo_white = (255,222,173)
+        self.spring_green = (0,255,127)
         # https://www.rapidtables.com/web/color/RGB_Color.html
 
         # Window
@@ -52,6 +54,9 @@ class Graphics:
         self.init_pint_electrodes_y = int(self.height/2) - int((self.num_divices/2)*(self.electro_h+self.distance_between))
         self.electrodes_rect_list = []
         self.rect_flag_list = []
+
+        self.bottons_rect_list = []
+        self.bottons_text_list = []
 
         self.font_electrodes = pygame.font.Font(None, 25)
         self.electrodes_text_list = []
@@ -78,6 +83,32 @@ class Graphics:
             text_pos.center = ( x_pos+int(self.electro_w/2), y_pos + int(self.electro_h/2) )
             self.electrodes_text_list.append({"id": i, "text": text, "text_pos": text_pos })
 
+
+        # create bottoms        
+        botton_width = 110
+        botton_height = 50
+        x_pos = self.width - (1.5*botton_width)
+        y_pos = int(self.height/3)
+        dist_bottons = 1.3*botton_height
+
+        self.bottons_rect_list.append({"id": 0, "rect": pygame.Rect((x_pos, y_pos), (botton_width, botton_height)), "color": self.light_coral})                         # Delete
+        self.bottons_rect_list.append({"id": 1, "rect": pygame.Rect((x_pos, y_pos+dist_bottons), (botton_width, botton_height)), "color": self.light_sky_blue})        # Clean
+        self.bottons_rect_list.append({"id": 2, "rect": pygame.Rect((x_pos, y_pos+2*dist_bottons), (botton_width, botton_height)), "color": self.spring_green})       # Start
+
+        text = self.font_electrodes.render("Delete", 1, self.black )
+        text_pos = text.get_rect()
+        text_pos.center = ( x_pos+int(botton_width/2), y_pos + int(botton_height/2) )
+        self.bottons_text_list.append({"text": text, "text_pos": text_pos })
+
+        text = self.font_electrodes.render("Clear", 1, self.black )
+        text_pos = text.get_rect()
+        text_pos.center = ( x_pos+int(botton_width/2), y_pos+dist_bottons + int(botton_height/2) )
+        self.bottons_text_list.append({"text": text, "text_pos": text_pos })
+
+        text = self.font_electrodes.render("Start", 1, self.black )
+        text_pos = text.get_rect()
+        text_pos.center = ( x_pos+int(botton_width/2), y_pos+2*dist_bottons + int(botton_height/2) )
+        self.bottons_text_list.append({"text": text, "text_pos": text_pos })
 
         # Draw stored numbers (electrodes's ids)
         self.font_select_ids = pygame.font.Font(None, 36)
@@ -114,15 +145,23 @@ class Graphics:
             else:
                 color_rect =  self.navajo_white
 
-            # pygame.draw.rect(self.map, self.navajo_white, rect_electro["rect"] )
             pygame.draw.rect(self.map, color_rect, rect_electro["rect"] )
 
+        # draw Electrodes text
         for text_electro in self.electrodes_text_list:
-            # draw rect text
             self.map.blit(text_electro["text"], text_electro["text_pos"])
+
 
         for text_path in self.path_text_list:
             self.map.blit(text_path["text"], text_path["text_pos"])
+
+
+        # Bottons
+        for botton in self.bottons_rect_list:
+            pygame.draw.rect(self.map, botton["color"], botton["rect"] )
+        
+        for text_botton in self.bottons_text_list:
+            self.map.blit(text_botton["text"], text_botton["text_pos"])
 
 
         # Delete path botton
@@ -130,6 +169,19 @@ class Graphics:
         #                     pygame.Rect( rect_electro[0], rect_electro[1], rect_electro[2], rect_electro[3] ))
             
     
+    def click_over_electrodes(self, x, y):
+
+        for rect_electro in self.electrodes_rect_list: 
+            if rect_electro["rect"].collidepoint(x, y):    
+                id_electro = rect_electro["id"] 
+                print("Click over electrode = ", id_electro)
+                
+                self.path_list.append(id_electro)
+                self.rect_flag_list[id_electro] = 1
+
+                # store the id selected to draw the text
+                self.number2text_store(id_electro)
+
 
     def dectect_actions(self):
         running = True
@@ -147,16 +199,8 @@ class Graphics:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = pygame.mouse.get_pos()                       # Get click position
 
-                for rect_electro in self.electrodes_rect_list: 
-                    if rect_electro["rect"].collidepoint(x, y):    
-                        id_electro = rect_electro["id"] 
-                        print("Click over electrode = ", id_electro)
-                        
-                        self.path_list.append(id_electro)
-                        self.rect_flag_list[id_electro] = 1
+                self.click_over_electrodes(x, y)
 
-                        # store the id selected to draw the text
-                        self.number2text_store(id_electro)
 
         return running
     
